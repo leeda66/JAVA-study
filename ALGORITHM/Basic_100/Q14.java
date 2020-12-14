070d81ffaa06997d79bbe00e99bcfd4f26d6fd8e
dahyun <ekgus2004@naver.com>
2020-12-15 00:40:14 +0900

    input and output

---
+++ ALGORITHM/Basic_100/Q14.java
@@ -0,0 +1,12 @@
+package Basic_100;
+import java.util.Scanner;
+
+public class Q14{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+
+        System.out.printf("%s %s", b, a);
+    }
+}
