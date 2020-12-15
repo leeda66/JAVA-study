9db83086c905bfd6ee9db6c27af7d40d012e66da
dahyun <ekgus2004@naver.com>
2020-12-15 23:48:19 +0900

    input and output

---
+++ ALGORITHM/Basic_100/Q15.java
@@ -0,0 +1,10 @@
+package Basic_100;
+import java.util.Scanner;
+
+public class Q15{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        double a = Double.parseDouble(scan.nextLine());
+        System.out.printf("%.2f", a);
+    }
+}
