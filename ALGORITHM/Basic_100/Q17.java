d409d48a256e6842bd00843824ab5cc5b93130af
dahyun <ekgus2004@naver.com>
2021-01-04 10:26:00 +0900

    input and output

---
+++ ALGORITHM/Basic_100/Q17.java
@@ -0,0 +1,11 @@
+package Basic_100;
+
+import java.util.Scanner;
+
+public class Q17{
+public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.printf("%d %d %d", a, a, a);
+        }
+}
