f6243863122c739bd269e12aec0dc696e8de9ea9
dahyun <ekgus2004@naver.com>
2021-01-05 23:54:09 +0900

    data type

---
+++ ALGORITHM/Q28.java
@@ -0,0 +1,10 @@
+package Basic_100;
+import java.util.Scanner;
+
+public class Q28 {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        long num = scan.nextLong();
+        System.out.print(num);
+    }
+}
