90ce23ceec27e495b09c1fa0ca8c313f9905deec
dahyun <ekgus2004@naver.com>
2021-01-06 12:42:41 +0900

    data type

---
+++ ALGORITHM/Basic_100/Q29.java
@@ -0,0 +1,10 @@
+package Basic_100;
+import java.util.Scanner;
+
+public class Q29 {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        double num = scan.nextDouble();
+        System.out.printf("%.11f", num);
+    }
+}
